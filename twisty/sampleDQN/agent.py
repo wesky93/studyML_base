e6b0c1922e82bbf5f17b26dc00f4ba534de8524b
sinsky <wesky93@gmail.com>
2017-01-22 22:44:16 +0900

    batch size 조절

--- twisty/sampleDQN/agent.py
+++ twisty/sampleDQN/agent.py
@@ -16,7 +16,7 @@ n_action = 3
 size = 144
 # 총 진행할 게임 횟수
 episode = 5000
-batch = 1000
+batch = 200
 # 한게임당 큐브 회전 횟수 제한
 max_play = 100
 # 게임 진행 횟수
@@ -38,7 +38,7 @@ def main( _ ) :
             # 결정한 액션을 이용해 게임을 진행하고, 보상과 게임의 종료 여부를 받아옵니다.
             reward, gameover = game.proceed( np.argmax( action ) )
             # 점수를 받을 경우 출력
-            if reward >= 8 :
+            if reward >= 12 :
                 print( game.total_game, '번 게임의 ', game.cube.count, '번째 회전에서 ', reward, '점 획득!!' )
             # 위에서 결정한 액션에 따른 현재 상태를 가져옵니다.
             # 상태는 screen_width x screen_height 크기의 화면 구성입니다.
@@ -46,6 +46,7 @@ def main( _ ) :
 
             # DQN 으로 학습을 진행합니다.
             brain.step( state, action, reward, gameover )
+
         if game.cube.done:
             print(game.total_game,'번째에서',game.cube.count,'회전만으로 큐브 완성!')
         if game.total_game % batch == 0:
