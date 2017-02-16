1f52baf6abb465d72082dc4fcb1e81d2d835e7ab
sinsky <wesky93@gmail.com>
2017-02-16 12:56:22 +0900

    lab4 커밋

--- twisty/myRL_0/agent.py
+++ twisty/myRL_0/agent.py
@@ -23,10 +23,10 @@ max_play = 50
 # 테스트 배치 사이즈
 test_batch_size = 100
 # 스크램 길이 설정
-scram_size = 10
+scram_size = 5
 
 # 실험 이름(logs 기록에 사용됨)
-lab = 'lab3'
+lab = 'lab4'
 
 # 테스트 배치 기록
 test_batch_record = { '학습정보' : { '스크램길이' : scram_size, '최대회전' : max_play, '배치사이즈' : batch,'테스트배치사이즈':test_batch_size } }
