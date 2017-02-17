7903006e5c6f7addbd2ab91b349b0c88c0e3e2b1
sinsky <wesky93@gmail.com>
2017-02-17 15:06:26 +0900

    결과를 파일로 로깅하는 부분 제거
    텐서보드에 모든 것을 기록하기에 성능 향상 차원에서 파일에 기록하던 부분을 제거

--- twisty/myRL_0/agent.py
+++ twisty/myRL_0/agent.py
@@ -28,21 +28,6 @@ scram_size = 5
 # 실험 이름(logs 기록에 사용됨)
 lab = 'lab5'
 
-# 테스트 배치 기록
-test_batch_record = {
-    '학습정보' : { '스크램길이' : scram_size, '최대회전' : max_play, '배치사이즈' : batch, '테스트배치사이즈' : test_batch_size }
-    }
-
-
-def logging( log, file ) :
-    folder = 'train_log'
-    if not path.isdir( folder ) :
-        os.makedirs( folder )
-    train_day = time.strftime( '%Y-%m-%d-' )
-    with open( path.join( folder, '{}{}'.format( train_day, file ) ), mode='a' ) as logs :
-        logs.write( '\n{}'.format( log ) )
-
-
 def test( scram_size, max_play, DQN, batch_size=100 ) :
     """
     학습 결과를 측정하기 위해 여러개의 시뮬레이션을 돌린뒤 평균을 반환한다.
@@ -84,67 +69,57 @@ def test( scram_size, max_play, DQN, batch_size=100 ) :
 
 
 def main( _ ) :
-    try :
-        # logname = input( "로그 파일 명을 입력하세요!" )
-        logname = lab
-        game = Games( scram_size, max_play )
-        brain = cubeDQN( game.set, cube_size=game.size, lab=lab )
-        # 테스트 실행 횟수
-        test_run_count = 0
+    # logname = input( "로그 파일 명을 입력하세요!" )
+    logname = lab
+    game = Games( scram_size, max_play )
+    brain = cubeDQN( game.set, cube_size=game.size, lab=lab )
+    # 테스트 실행 횟수
+    test_run_count = 0
 
-        # 시간 측정
-        start = time.time( )
-        print( 'start training' )
-        while 1 :
-            game.reset( )
-            # todo: 처음 게임을 시작할때는 getaction시 상태값을 보내게 만들고 이후엔 상태값 입력없이 진행하게 한다.
-            train = True
-            # 처음 진행할 경우 현재 상태를 이용하여 액션값을 가져온다
-            acts = brain.get_action( state=game.states, train=train )
-            # 게임 진행 여부
-            end = False
+    # 시간 측정
+    start = time.time( )
+    print( 'start training' )
+    while 1 :
+        game.reset( )
+        # todo: 처음 게임을 시작할때는 getaction시 상태값을 보내게 만들고 이후엔 상태값 입력없이 진행하게 한다.
+        train = True
+        # 처음 진행할 경우 현재 상태를 이용하여 액션값을 가져온다
+        acts = brain.get_action( state=game.states, train=train )
+        # 게임 진행 여부
+        end = False
 
-            while not end :
-                # DQN 모델을 이용해 실행할 액션을 결정합니다.
-                action = game.set[ acts ]
-                # 결정한 액션을 이용해 게임을 진행하고, 보상과 게임의 종료 여부를 받아옵니다.
-                reward, gameover = game.proceed( action )
+        # 게임 진행
+        while not end :
+            # DQN 모델을 이용해 실행할 액션을 결정합니다.
+            action = game.set[ acts ]
+            # 결정한 액션을 이용해 게임을 진행하고, 보상과 게임의 종료 여부를 받아옵니다.
+            reward, gameover = game.proceed( action )
 
-                # DQN 으로 학습을 진행한뒤 다음 액션값을 받아온다.
-                acts = brain.step( game.states, acts, game.reward, train )
-                # 게임이 종료되면 반복을 끝낸다.
-                if gameover :
-                    end = True
+            # DQN 으로 학습을 진행한뒤 다음 액션값을 받아온다.
+            acts = brain.step( game.states, acts, game.reward, train )
+            # 게임이 종료되면 반복을 끝낸다.
+            if gameover :
+                end = True
 
-            if game.total_game % batch == 0 :
-                # 각 배치 실행 시간 측정
-                end = time.time( )
-                runtime = end - start
+        # 한 게임이 끝난뒤 결과를 기록한다
+        if game.total_game % batch == 0 :
+            # 각 배치 실행 시간 측정
+            end = time.time( )
+            runtime = end - start
 
-                # 학습 결과 테스트
-                test_run_count += 1
-                test_count, test_reward, test_done = test( scram_size, max_play, brain, batch_size=test_batch_size )
-                # 텐서보드에 테스트 결과 기록
-                brain.reward_log( test_run_count * test_batch_size, test_reward, test_count, test_done )
-                # 콘솔에 테스트 결과 출력
-                result = { "평균회전횟수" : test_count, "평균보상" : test_reward, "완성확률" : test_done }
-                # 테스트 결과 기록
-                test_batch_record[ test_run_count ] = result
-
-                batch_state = "==== 테스트 결과 ====\n" \
-                              "게임 진행횟수: {}, 평균보상: {}, 큐브 완성 확률: {}\n" \
-                              "평균 회전 횟수: {}, 소요시간: {}" \
-                    .format( game.total_game, test_reward, test_done,
-                             test_count, runtime )
-                print( batch_state )
-                start = time.time( )
-    except Exception as e :
-        raise e
-    finally :
-        # 프로그램 종료시 테스트 기록을 로깅 및 출력한다.
-        logging( test_batch_record, logname )
-        print( test_batch_record )
-
+            # 학습 결과 테스트
+            test_run_count += 1
+            test_count, test_reward, test_done = test( scram_size, max_play, brain, batch_size=test_batch_size )
+            # 텐서보드에 테스트 결과 기록
+            brain.reward_log( test_run_count * test_batch_size, test_reward, test_count, test_done )
+            # 콘솔에 테스트 결과 출력
+            bach_state = "==== 테스트 결과 ====\n" \
+                          "게임 진행횟수: {}, 평균보상: {}, 큐브 완성 확률: {}\n" \
+                          "평균 회전 횟수: {}, 소요시간: {}" \
+                .format( game.total_game, test_reward, test_done,
+                         test_count, runtime )
+            print( batch_state )
+            start = time.time( )
 
 if __name__ == '__main__' :
     tf.app.run( )
